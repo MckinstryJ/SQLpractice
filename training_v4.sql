45b648c75ccbc7da0ec8e3e39add66ee713739a2
John <Xzten3@gmail.com>
2021-05-24 20:28:41 -0600

    Add files via upload

--- training_v4.sql
+++ training_v4.sql
@@ -25,3 +25,27 @@ drop user 'bob'@'localhost';
 
 -- see all system variables
 SHOW VARIABLES WHERE Variable_Name LIKE '%dir';
+
+-- run mysql command in command prompt
+-- 1. move to MySQL Server \ bin
+-- 2. run 'mysql -e "select * from sql_invoicing.customer_payment_totals" -u bob -p
+-- this will ask for bob's login password and the sql will show as a table
+
+-- doing the same but pushing to file
+-- mysql.exe -e "select * from sql_invoicing.customer_payment_totals" 
+-- -u john -p > 
+-- C:\Users\xzten\Desktop\Databases\MySQL\SQL Course Materials\customer.txt
+
+-- can do it with a dump aka data export
+
+-- actual dump command
+-- run 'mysqldump -u john -p sql_hr employees > C:\Users\xzten\backups\customer.sql'
+
+-- journaling - continues to grow until database is backed up
+-- committing the data is called "truncating" the log file
+
+-- everything done is saved to a binary log
+-- can completely reproduce the database (will contain usernames, passwords, etc)
+
+-- Creating a Trigger
+...get video link in lab notebook...
